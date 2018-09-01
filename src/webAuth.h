ed4507aca4ec7f2059a02e5eae7b9323c54251c4
Zhensheng Yuan <extremebored@gmail.com>
2018-09-02 01:48:19 +0800

    Update edubas

--- src/webAuth.h
+++ src/webAuth.h
@@ -10,7 +10,7 @@
 #define AUTH_POST_URL "http://enet.10000.gd.cn:10001/login.do"
 #define AUTH_FAIL_URL "http://enet.10000.gd.cn:10001/failed.jsp"
 // #define AUTH_POST_URL "http://192.168.1.1/login.do"
-#define EDUBAS "113.98.10.136"
+#define EDUBAS "113.98.14.153"
 #define SUCCESS_RETURN "http://enet.10000.gd.cn:10001/success.jsp"
 
 struct webAuth;
